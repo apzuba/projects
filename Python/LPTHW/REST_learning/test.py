688b4488780dc17523e17b061ca656ce4b58f8dd
Adam Zuba <72082068+apzuba@users.noreply.github.com>
2023-01-23 15:16:28 +0100

    Add files via upload

---
+++ Python/LPTHW/REST_learning/test.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+response = requests.get(
+    "https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow")
+
+# print(response.json()['items'])
+
+for question in response.json()['items']:
+    if question['answer_count'] == 0:
+        print(question['title'])
+        print(question['link'])
+    else:
+        print('skipped')
+    print()
