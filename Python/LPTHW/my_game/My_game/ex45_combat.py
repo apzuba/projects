95960c99ca12f5d9e325a6e9d79dad5e266e5ff1
Adam Zuba <72082068+apzuba@users.noreply.github.com>
2023-01-27 13:20:53 +0100

    Update ex45_combat.py

--- Python/LPTHW/my_game/My_game/ex45_combat.py
+++ Python/LPTHW/my_game/My_game/ex45_combat.py
@@ -25,12 +25,14 @@ class Combat():
             print("--- coś poszło nie tak---")
 
 
-
+    #list of all the available user attacks
     attacks = ['magic-spell', 'laser-katana', 'laser-pistol', 'pee', 'smile', 'scream', '30 UKL']
 
+    #list of all the monsterns
     shoguns_list = ['Shoghun-ghul', 'Ronin', 'Ninja', 'Unspecified-Undead',
                     'Space-assasin', 'Zombie', 'Uzaki-Monk', 'Undead-deity']
 
+    #a dict with the monsterns and their corresponding weaknesses
     shoguns_base = {
                     'Shoghun-ghul': 'laser-pistol',
                     'Ronin': 'laser-katana',
@@ -46,8 +48,10 @@ class Combat():
     def shogun_attack(self, armor):
         self.armor = armor
 
+        #randomly loads the monstern
         shoguns_type = Combat.shoguns_list[randint(0,7)]
 
+        #HP points handling
         if self.armor == 1:
             health_points = 1
         elif self.armor == 2:
@@ -66,9 +70,11 @@ class Combat():
             sys.stdout.write(i)
             time.sleep(0.03)
 
+        #attack choice + check of the attack effectiveness
         while True:
             attack = input("Co robisz? > ")
 
+            #list of communicates when the attack didn't work
             Not_effective = [
                             f"Uh, {attack} nie było zbyt efektywne...",
                             f"{attack} - nie zadziałało...",
@@ -78,6 +84,7 @@ class Combat():
             att_not_eff = Not_effective.pop(randint(0,len(Not_effective)-1))
             Not_effective.append(att_not_eff)
 
+            #list of communicates when the attack worked
             Effective = [
                             f"Tak, {attack} zadziałało świetnie!",
                             f"Rozwaliło {shoguns_type} na strzępy!",
@@ -85,7 +92,10 @@ class Combat():
                             ]
             att_eff = Effective.pop(randint(0,len(Effective)-1))
             Effective.append(att_eff)
-
+            
+            #checks if the chosen attack corresponds to the monstern's weakness 
+            
+            #the attack worked, the user earns a random amount of gold
             if attack == Combat.shoguns_base.get(shoguns_type):
 
                 for i in dedent(f"""
@@ -100,9 +110,11 @@ class Combat():
                 print(f"Znalazłeś przy potworze {gold} UKL. Masz teraz {ex45_game_stats.game_stats.pocket_money} UKL.")
                 return False
 
+            #quit the game mid-fight hidden option
             elif attack == "quit" or attack == "q":
                 exit(0)
 
+            #buy the freedom for 30 gold (if the user has it)
             elif attack == "30UKL" or attack == "30" or attack == "30 UKL" or attack == "30 ukl":
                 if ex45_game_stats.game_stats.pocket_money > 30:
 
@@ -118,7 +130,7 @@ class Combat():
                 else:
                     print("Nie masz tyle pieniędzy.")
 
-
+            #Wrong attack choice - user loses 1HP (either dies or has another chance)
             elif attack != Combat.shoguns_base.get(shoguns_type)\
                 and health_points != 0:
                 health_points -= 1
@@ -133,11 +145,13 @@ class Combat():
                     sys.stdout.write(i)
                     time.sleep(0.02)
 
+                #if user barely survived, his HP stats setting will decrease
                 if health_points == 1:
                     ex45_game_stats.game_stats.hard_damage = True
                 else:
                     pass
-
+                
+            #the user died
             else:
                 print(f"\nSHokomotota! {shoguns_type} zabija cię. :<\n")
                 Death.gogo()
