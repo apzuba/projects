3713da327b277924cbb994bcf3e469865c96594d
Adam Zuba <72082068+apzuba@users.noreply.github.com>
2023-01-27 13:11:21 +0100

    Update imie_script.py

--- Python/LPTHW/my_game/My_game/imie_script.py
+++ Python/LPTHW/my_game/My_game/imie_script.py
@@ -1,12 +1,8 @@
+#A short module to get the name of the player and use in every module.
+
 class Run_imie():
     def run_imie():
         imie_ask = input("What is your name? >")
         return(imie_ask)
 
 imie_1 = Run_imie.run_imie()
-
-
-#what I want to do:
-#1.Run this script in ex45a  <-- where is it saving the imie_1 result?
-#2.Use the result from ex45a in gen_kompl.
-# -I can't do ex45a import in gen_kompl.
