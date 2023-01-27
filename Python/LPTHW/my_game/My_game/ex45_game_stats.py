07dd9751f1f8037cb7c428a4e174b988b7495f86
Adam Zuba <72082068+apzuba@users.noreply.github.com>
2023-01-27 13:07:09 +0100

    Update ex45_game_stats.py

--- Python/LPTHW/my_game/My_game/ex45_game_stats.py
+++ Python/LPTHW/my_game/My_game/ex45_game_stats.py
@@ -3,12 +3,13 @@ import time
 
 from random import randint
 
-#HEALTH POINTS ITEMS UPDATING FUNCTION
+#The function to update the game stats
 def game_stats():
     defence = 2
     pocket_money = 0
     hard_damage = False
 
+#The initial game stats settings
 game_stats.defence = 3
 game_stats.pocket_money = int(randint(25,55))
 game_stats.hard_damage = False
