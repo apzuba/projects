0b220733a75a35a07036d3763de67410146829e8
Adam Zuba <72082068+apzuba@users.noreply.github.com>
2023-02-03 15:38:39 +0100

    Update example_SQL_commands.sql

--- example_SQL_commands.sql
+++ example_SQL_commands.sql
@@ -1,22 +1,22 @@
-#1
+/*1 Basic inserting function.*/
 INSERT INTO Users (name, email) VALUES ('Chuck', 'csev@umich.edu') ;
 INSERT INTO Users (name, email) VALUES ('Sally', 'sally@umich.edu') ;
 
 
-#2
+/*2 Basic table creation function.*/
 CREATE TABLE ___ ( attribute1 VARCHAR(128), attribute2 VARCHAR(128) );
 user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 PRIMARY KEY(user_id),
 
 
-#3
+/*3 Distinct selection with groupping */
 SELECT DISTINCT Artist.name, COUNT(DISTINCT Genre.name)
 FROM Track JOIN Album JOIN Artist JOIN Genre
 ON Album.artist_id = Artist.artist_id AND Track.album_id = Album.album_id AND Track.genre_id = Genre.genre_id
 GROUP BY Artist.name;
 
 
-#4
+/*4 Establishing a foreign key.*/
 CREATE TABLE Artist (
     artist_id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
@@ -36,7 +36,7 @@ CREATE TABLE Album (
     ) ENGINE = InnoDB;
 
 
-#5
+/*5 Creating slightly more complex tables and establishing a foreign key in a hierarchical order */
 CREATE TABLE Genre (
      genre_id INTEGER NOT NULL AUTO_INCREMENT,
      name VARCHAR(255),
